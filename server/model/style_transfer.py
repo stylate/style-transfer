2f9fa2b8887e10500bcea5a8235049e786ab1206
Alan Nguyen <nguyenalan@berkeley.edu>
2020-04-17 20:00:37 -0700

    style transfer draft

---
+++ server/model/style_transfer.py
@@ -0,0 +1,71 @@
+# pytorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+# misc
+import utils
+from PIL import Image
+
+def process(content_path, style_path, iterations=500):
+    # init VGG model
+    vgg = models.vgg19(pretrained=True)
+    for param in vgg.parameters():
+        param.requires_grad_(False)
+
+    for i, layer in enumerate(vgg.features):
+        if isinstance(layer, torch.nn.MaxPool2d):
+            vgg.features[i] = torch.nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    vgg.to(device).eval()
+
+    # init content and style image + features
+    content = utils.load_image(content_path).to(device)
+    style = utils.load_image(style_path).to(device)
+
+    content_features = utils.get_features(content, vgg)
+    style_features = utils.get_features(style, vgg)
+
+    content_weight = 1e4
+    style_weight = 1e2
+    target = torch.randn_like(content).requires_grad_(True).to(device)
+
+    # gradient descent
+    optimizer = optim.Adam([target], lr=0.01)
+    style_weights = {
+        'conv1_1': 0.75,
+        'conv2_1': 0.5,
+        'conv3_1': 0.2,
+        'conv4_1': 0.2,
+        'conv5_1': 0.2
+    }
+    style_grams = {
+        layer: utils.gram_matrix(style_features[layer]) for layer in style_features
+    }
+
+    for i in range(1, iterations + 1):
+        optimizer.zero_grad()
+        target_features = utils.get_features(target, vgg)
+
+        content_loss = torch.mean((target_features['conv4_2'] - content_features['conv4_2']) ** 2)
+
+        style_loss = 0
+        for layer in style_weights:
+            target_feature = target_features[layer]
+            target_gram = utils.gram_matrix(target_feature)
+            _, dim, h, w = target_feature.shape
+
+            style_gram = style_grams[layer]
+            layer_style_loss = style_weights[layer] * torch.mean((target_gram - style_gram) ** 2)
+            style_loss += layer_style_loss / (dim * h * w)
+
+            total_loss = (content_weight * content_loss) + (style_weight * style_loss)
+            total_loss.backward(retain_length=True)
+            optimizer.step()
+
+    final_img = utils.deprocess(target)
+    return final_img
---
+++ server/model/utils.py
@@ -0,0 +1,56 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torch.optim as optim
+from torchvision import transforms, models
+
+VGG_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+VGG_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
+
+def load_image(img_path, max_size=400, shape=None):
+    image = Image.open(img_path).convert('RGB')
+    size = max(max_size, max(image.size))
+    if shape: size = shape
+
+    transform = transforms.Compose([
+        transforms.Resize((size, int(1.5*size))),
+        transforms.ToTensor(),
+        transforms.Normalize(VGG_MEAN.tolist(), VGG_STD.tolist())]
+    )
+
+    return transform(image)[:3, :, :].unsqueeze(0) # return as tensor: batch dim, rgb channels, height, width
+
+def deprocess(tensor):
+    # convert tensor to numpy array and RGB dimensions
+    image = tensor.to("cpu").clone().detach()
+    image = image.numpy().squeeze()
+    image = image.transpose(1, 2, 0)
+    image = image * VGG_STD + VGG_MEAN
+    image = image.clip(0, 1)
+    return image
+
+def get_features(image, model):
+    layers = {
+        '0': 'conv1_1',
+        '5': 'conv2_1',
+        '10': 'conv3_1',
+        '19': 'conv4_1',
+        '21': 'conv4_2',
+        '28': 'conv5_1'
+    }
+    features = {}
+    x = image
+
+    for name, layer in enumerate(model.features):
+        x = layer(x)
+        if str(name) in layers:
+            features[layers[str(name)]] = x
+
+    return features
+
+def gram_matrix(tensor):
+    _, n_filters, h, w = tensor.size()
+    tensor = tensor.view(n_filters, h * w)
+    return torch.mm(tensor, tensor.t())
